5f6d551555a7990d65df706f28e0f518efa01d44
Alexandre Marreiros <amarreiros@gmail.com>
2022-10-31 12:14:32 +0000

    file with code
    Copy recursively files from a folder to a new one considering the creation of a folder with the same name of the file

---
+++ copyfilesrecursive.ps1
@@ -0,0 +1,13 @@
+﻿
+
+      
+
+     $sourceFolderPath = "c:\dell"
+     $destinationFolderPath = "c:\example3"
+     if (!(Test-Path -path $destinationFolderPath)) {New-Item $destinationFolderPath -Type Directory}
+     foreach($file in (Get-ChildItem -path $sourceFolderPath -Recurse -File )){        
+         $destinationSubFolderPath = $destinationFolderPath + "\" +[System.IO.Path]::GetFileNameWithoutExtension($file.Name)    
+         if (!(Test-Path -path $destinationSubFolderPath)) {New-Item $destinationSubFolderPath -Type Directory}    
+         Copy-Item $file.FullName -Destination  $destinationSubFolderPath   
+     }
+
